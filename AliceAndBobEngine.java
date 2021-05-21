42a31e28128a87e74d136ad254c60cebf42c35ae
Leon Christopher Hunter <xleonhunter@gmail.com>
2021-05-21 11:34:48 -0400

    Initial commit

---
+++ AliceAndBobEngine.java
@@ -0,0 +1,22 @@
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 5/19/21 5:05 PM
+ */
+public class AliceAndBobEngine {
+    public Boolean isAlice(String input) {
+        return null;
+    }
+
+    public Boolean isBob(String input) {
+        return null;
+    }
+
+    public Boolean isAliceOrBob(String input) {
+        return null;
+    }
+
+    public String getGreeting(String input) {
+        return null;
+    }
+}
